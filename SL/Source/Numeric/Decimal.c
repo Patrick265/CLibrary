b6ba05cf08263e5f8aed3e7019a3cad6ce15235f
Patrick265 <Patrick.dejong@protonmail.com>
2022-02-03 09:04:36 +0100

    Fixed Whitespace

--- SL/Source/Numeric/Decimal.c
+++ SL/Source/Numeric/Decimal.c
@@ -5,7 +5,7 @@
 
 bool FloatApproxEqualWithEpsilon(f32 a, f32 b, float epsilon)
 {
-    return fabs(a - b) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+	return fabs(a - b) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
 }
 bool FloatApproxEqual(f32 a, f32 b)
 {
